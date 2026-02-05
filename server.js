34e95c7841aa4df4b3f499061fc29be6e438c811
eru038 <erueru038@gmail.com>
2026-02-05 14:05:52 +0900

    Change DB connection to use DATABASE_URL
    Updated database connection configuration to use DATABASE_URL.

--- server.js
+++ server.js
@@ -289,11 +289,12 @@ app.post('/logout', (req, res) => {
 const { Client } = require("pg");
 
 const db = new Client({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  // host: process.env.DB_HOST,
+  // port: process.env.DB_PORT,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASS,
+  // database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false } 
 });
 
